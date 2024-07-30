ece7561a12a8ef01732252769d7b7eef9e1a1615
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-30 20:01:05 +0530

    Database setup

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import { createServer } from "http";
 
+import { loginRouter } from "./routes/login-router";
+
 const app = express();
 const server = createServer(app);
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use("/api/login", loginRouter);
 
 app.get("/", (_, res) => {
   return res.send("Hello World");
