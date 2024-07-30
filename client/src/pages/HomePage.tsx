a3cc77b1f78376bb8b406685303240fddc209c25
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-31 02:04:46 +0530

    shadcn added

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
     <div className="flex items-center justify-center w-full h-full text-red-600 text-5xl font-extrabold">
-      HomePage
+      <Button>HomePage</Button>
     </div>
   );
 };
