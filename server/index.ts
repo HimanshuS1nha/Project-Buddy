634bbbf0044a6edaecb930291baad4c6adcfa3ea
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-31 13:46:48 +0530

    login router bug fixes

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createServer } from "http";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { loginRouter } from "./routes/login-router";
 import { signupRouter } from "./routes/signup-router";
@@ -10,6 +11,7 @@ const server = createServer(app);
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/login", loginRouter);
--- server/routes/login-router.ts
+++ server/routes/login-router.ts
@@ -35,7 +35,11 @@ loginRouter.post("/", async (req, res) => {
       maxAge: 60 * 1000 * 60 * 24 * 30,
     });
 
-    return res.status(200).json({ message: "Logged in successfully" });
+    const { password: _, createdAt, updatedAt, ...restUser } = user;
+
+    return res
+      .status(200)
+      .json({ message: "Logged in successfully", user: restUser });
   } catch (error) {
     if (error instanceof ZodError) {
       return res.status(422).json({ error: error.errors[0].message });
