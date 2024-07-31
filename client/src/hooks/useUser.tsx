adac0535817c53b5c149a7b68ea41c6ece62d6fe
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-31 13:41:56 +0530

    useUser hook added

--- client/src/hooks/useUser.tsx
+++ client/src/hooks/useUser.tsx
@@ -1,1 +1,13 @@
-import {} from "zustand";
+import { create } from "zustand";
+
+import { UserType } from "types";
+
+type UseUserType = {
+  user: UserType | null;
+  setUser: (user: UserType | null) => void;
+};
+
+export const useUser = create<UseUserType>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+}));
